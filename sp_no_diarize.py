3b880f34fd5d8d643ef45703907f7fe2fb6ef079
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2016-10-20 22:07:52 +0800

    successful sox integration

--- sp_no_diarize.py
+++ sp_no_diarize.py
@@ -3,6 +3,7 @@ import json
 import os
 
 from googleapiclient.discovery import build
+import sox
 
 # init
 cur_dir = os.path.dirname(os.path.realpath(__name__))
@@ -12,10 +13,21 @@ service = build('speech', 'v1beta1', developerKey=API_KEY)
 speech = service.speech()
 
 
-def recognize_file(filename):
-    path = os.path.join(data_dir, filename)
-    with open(path, 'rb') as file:
-        content = base64.b64encode(file.read())
+def convert(file):
+    filename = os.path.splitext(file)[0]
+    path_in = os.path.join(data_dir, file)
+    path_out = os.path.join(data_dir, filename + '-resampled.wav')
+    tfm = sox.Transformer()
+    tfm.channels(1)
+    tfm.rate(16000)
+    tfm.build(path_in, path_out)
+    return path_out
+
+
+def recognize_file(file):
+    path = os.path.join(data_dir, file)
+    with open(path, 'rb') as f:
+        content = base64.b64encode(f.read())
         content_string = content.decode('utf-8')
     request_body = {
         "audio": {
@@ -31,5 +43,4 @@ def recognize_file(filename):
     response = request.execute()
     return response
 
-response = print(recognize_file('test-2.wav'))
-print(response)
+print(recognize_file('test-resampled.wav'))
