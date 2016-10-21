55016658d829c880bea4d505c5c8c924b8406d2c
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2016-10-21 14:37:23 +0800

    minor refactors

--- speech_apiv1.py
+++ speech_apiv1.py
@@ -12,11 +12,12 @@ data_dir = os.path.join(cur_dir, 'data/')
 API_KEY = 'AIzaSyC22qOuouVqsraoV6KzCHNAzdf3gWisOwc'
 service = build('speech', 'v1beta1', developerKey=API_KEY)
 speech = service.speech()
+operations = service.operations()
 
 
-def convert(file):
-    filename = os.path.splitext(file)[0]
-    path_in = os.path.join(data_dir, file)
+def convert(file_id):
+    filename = os.path.splitext(file_id)[0]
+    path_in = os.path.join(data_dir, file_id)
     path_out = os.path.join(data_dir, filename + '-resampled.wav')
     tfm = sox.Transformer()
     tfm.convert(samplerate=16000, n_channels=1, bitdepth=16)
@@ -24,8 +25,8 @@ def convert(file):
     return path_out
 
 
-def recognize_sync(file):
-    path = os.path.join(data_dir, file)
+def recognize_sync(file_id):
+    path = os.path.join(data_dir, file_id)
     with open(path, 'rb') as f:
         content = base64.b64encode(f.read())
         content_string = content.decode('utf-8')
@@ -39,13 +40,12 @@ def recognize_sync(file):
             "sampleRate": 16000
         },
     }
-    request = speech.syncrecognize(body=request_body)
-    response = request.execute()
+    response = speech.syncrecognize(body=request_body).execute()
     return response['results'][0]['alternatives'][0]['transcript']
 
 
-def recognize_async(file):
-    path = os.path.join(data_dir, file)
+def recognize_async(file_id):
+    path = os.path.join(data_dir, file_id)
     with open(path, 'rb') as f:
         content = base64.b64encode(f.read())
         content_string = content.decode('utf-8')
@@ -59,11 +59,10 @@ def recognize_async(file):
             "sampleRate": 16000
         },
     }
-    request = speech.asyncrecognize(body=request_body)
-    response = request.execute()
+    response = speech.asyncrecognize(body=request_body).execute()
     operation_id = response['name']
     for retries in range(100):
-        operation = service.operations().get(name=operation_id).execute()
+        operation = operations.get(name=operation_id).execute()
         if ('done' in operation.keys()):
             async_response = operation['response']
             return async_response['results'][0]['alternatives'][0]['transcript']
