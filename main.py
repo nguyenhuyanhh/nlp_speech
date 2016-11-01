ae2a554094d480f982c701770247c4f28aae7478
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2016-11-01 15:40:44 +0800

    deploy to main server

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ import speech
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-# data.flat_data('/home/nhanh/test')
+data.flat_data('/home/nhanh/work/raw')
 
 speech.diarize_workflow()
