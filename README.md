ee27ae498474bcfbcf0790574cb170cdba6a0082
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2016-11-10 16:18:39 +0800

    stats for completed folders

--- README.md
+++ README.md
@@ -54,8 +54,9 @@ The user can create any number of `/data*` folders as necessary, e.g. `/data_com
 Syntax: python data.py (option) (path)
     option:
         -i, --import: Import (path) into /data. (path) must only contain audio files i.e. must be flat
-        -m, --migrate: Migrate (path) from old /data structure to new /data structure.
+        -m, --migrate: Migrate (path) from old /data structure to new /data structure
         -c, --clear: Clear intermediate files from (path). (path) must follow /data structure (prescribed above/ in README)
+        -s, --stats: Output general stats about (path). Most useful for completed folders
     path: Path to the specified folder
 ```
 
--- data.py
+++ data.py
@@ -2,6 +2,7 @@ import shutil
 import os
 import logging
 import sys
+from decimal import Decimal
 
 from slugify import slugify
 
@@ -74,6 +75,39 @@ def migrate(path):
         shutil.move(old_textgrid, new_textgrid)
         logger.info('Processed %s', dir)
 
+
+def stats(path):
+    """
+    Return some statistics for the folder.
+    Useful for completion statistics.
+    """
+    # get list of folders that conforms to /data structure
+    key = set(['raw', 'resampled', 'diarization', 'transcript'])
+    completed_dirs = set()
+    for root, dirs, files in os.walk(path):
+        if key.issubset(dirs):
+            completed_dirs.add(root)
+
+    # get total time and no of file_ids processed
+    count = 0
+    time = Decimal(0.0)
+    for dir in completed_dirs:
+        resampled_dir = os.path.join(dir, 'resampled/')
+        if (len(os.listdir(resampled_dir)) == 1):
+            resampled_file = os.path.join(
+                resampled_dir, os.listdir(resampled_dir)[0])
+            time += Decimal(os.path.getsize(resampled_file)) / 32000
+            count += 1
+
+    # convert to human readable times
+    hours = int(time / 3600)
+    time -= hours * 3600
+    minutes = int(time / 60)
+    seconds = time - minutes * 60
+
+    logger.info('Processed %s files, total time %s hours %s minutes %s seconds.',
+                count, hours, minutes, seconds)
+
 if __name__ == '__main__':
     if len(sys.argv) != 3:
         logger.info('Invalid arguments. Exiting.')
@@ -83,5 +117,7 @@ if __name__ == '__main__':
         clear_intermediate(sys.argv[2])
     elif (sys.argv[1] in ['-m', '--migrate']):
         migrate(sys.argv[2])
+    elif (sys.argv[1] in ['-s', '--stats']):
+        stats(sys.argv[2])
     else:
         logger.info('Invalid arguments. Exiting.')
